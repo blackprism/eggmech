1ab5465e565d62a5383e4230e69b13ad4d32251c
srm <srm@bouh.org>
2024-06-08 20:10:11 +0200

    chore: wip (#7)

--- core/internal/bot.go
+++ core/internal/bot.go
@@ -5,102 +5,66 @@ import (
 	"log/slog"
 	"os"
 	"os/signal"
-	"time"
-
-	"eggmech/core"
 
 	"github.com/disgoorg/disgo"
 	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/events"
 	"github.com/disgoorg/disgo/gateway"
-	"github.com/nats-io/nats.go"
-	"github.com/nats-io/nats.go/jetstream"
+	"github.com/samber/oops"
+
+	"eggmech/core"
 )
 
-func Run(ctx context.Context, getenv func(string) string) int {
+func Run(ctx context.Context, getenv func(string) string) error {
 	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
 	defer cancel()
-	
-	nc, err := getNatsConnection()
-	
-	if err != nil {
-		return 1
-	}
-	
-	defer deferNc(nc)
-	
-	js, err := getJetstream(ctx, nc)
+
+	nc, err := core.Connect(getenv("NATS_URL"))
 
 	if err != nil {
-		return 1
+		return oops.Wrapf(err, "error connecting to nats server")
 	}
-	
-	client, err := disgo.New(getenv("DISCORD_TOKEN"),
+
+	defer core.Close(nc)
+
+	discord, err := disgo.New(getenv("DISCORD_TOKEN"),
 		bot.WithGatewayConfigOpts(
 			gateway.WithIntents(
 				gateway.IntentGuilds,
 				gateway.IntentGuildMessages,
 				gateway.IntentDirectMessages,
+				gateway.IntentGuildPresences,
 			),
 		),
-		bot.WithEventListenerFunc(MessageHandler(ctx, js)),
 	)
+
 	if err != nil {
-		slog.Error("Failed to create disgo", slog.Any("error", err))
-
-		return 1
-	}
-	// connect to the gateway
-	if err = client.OpenGateway(ctx); err != nil {
-		slog.Error("Failed to open gateway", slog.Any("error", err))
-
-		return 1
+		return oops.Wrapf(err, "error connecting to disgo")
 	}
 
-	slog.Info("bot is now running. Press CTRL-C to exit.")
+	js, err := core.JetstreamConnect(
+		ctx,
+		nc,
+		"ACTIVITY",
+		[]string{
+			"activity.>",
+		},
+	)
+
+	if err != nil {
+		return oops.Wrapf(err, "error connecting to Jetstream")
+	}
+
+	discord.AddEventListeners(&events.ListenerAdapter{
+		OnPresenceUpdate: core.PresenceHandler(ctx, js, discord.ID(), "activity"),
+	})
+
+	if err = discord.OpenGateway(ctx); err != nil {
+		return oops.Wrapf(err, "error connecting to Discord")
+	}
+
+	slog.Info("Bot is now running. Press CTRL-C to exit.")
 	<-ctx.Done()
 
-	return 0
-}
-
-func getNatsConnection() (*nats.Conn, error)  {
-	nc, err := nats.Connect(nats.DefaultURL)
-	
-	if err != nil {
-		slog.Error("Error connecting to nats", slog.Any("error", err))
-		return nil, err
-	}
-	
-	return nc, nil
-}
-
-func deferNc(nc *nats.Conn)  {
-	err := nc.Drain()
-	if err != nil {
-		slog.Error("Error draining nats", slog.Any("error", err))
-	}
-}
-
-func getJetstream(ctx context.Context, nc *nats.Conn) (jetstream.JetStream, error) {
-	js, err := jetstream.New(nc)
-	
-	if err != nil {
-		slog.Error("Error creating jetstream instance", slog.Any("error", err))
-		return nil, err
-	}
-
-	cfg := jetstream.StreamConfig{
-		Name:       core.Streams[0].Name,
-		Subjects:   core.Streams[0].Subjects,
-		MaxAge:     7 * 24 * time.Hour,
-		Duplicates: 10 * time.Second,
-		Storage:    jetstream.FileStorage,
-	}
-
-	_, err = js.CreateOrUpdateStream(ctx, cfg)
-	if err != nil {
-		slog.Error("Error creating stream", slog.Any("error", err))
-		return nil, err
-	}
-
-	return js, nil
+	return nil
 }
--- core/nats.go
+++ core/nats.go
@@ -1,15 +1,165 @@
 package core
 
-type Stream struct {
-	Name string
-	Subjects []string
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/jetstream"
+	"github.com/samber/oops"
+)
+
+const sleepBetweenConsumeFailure = 1 * time.Second
+const maxReconnects = -1
+
+var StreamConfigMaxAge = 7 * 24 * time.Hour
+var StreamConfigDuplicates = 10 * time.Second
+var StreamConfigStorage = jetstream.FileStorage
+
+func Connect(url string) (*nats.Conn, error) {
+	nc, err := nats.Connect(url, nats.MaxReconnects(maxReconnects))
+
+	if err != nil {
+		return nil, oops.Wrapf(err, "error connecting to nats")
+	}
+
+	return nc, nil
 }
 
-var Streams = []Stream{
-	{
-		Name: "MESSAGE",
-		Subjects: []string{
-			"message.>",
-		},
-	},
+func Close(nc *nats.Conn) {
+	if nc == nil {
+		return
+	}
+
+	err := nc.Drain()
+	if err != nil {
+		slog.Error("failed to drain nats connection", slog.Any("error", oops.Wrap(err)))
+	}
+
+	nc.Close()
+
+	return
+}
+
+func JetstreamConnect(ctx context.Context, nc *nats.Conn, name string, subjects []string) (jetstream.JetStream, error) {
+	js, err := jetstream.New(nc)
+
+	if err != nil {
+		return nil, oops.Wrapf(err, "error creating jetstream instance")
+	}
+
+	cfg := jetstream.StreamConfig{
+		Name:       name,
+		Subjects:   subjects,
+		MaxAge:     StreamConfigMaxAge,
+		Duplicates: StreamConfigDuplicates,
+		Storage:    StreamConfigStorage,
+	}
+
+	err = createOrUpdateStream(ctx, js, cfg)
+
+	if err != nil {
+		return nil, oops.Wrapf(err, "error creating or updating stream")
+	}
+
+	return js, nil
+}
+
+func streamExists(ctx context.Context, js jetstream.JetStream, cfg jetstream.StreamConfig) bool {
+	for streamName := range js.StreamNames(ctx).Name() {
+		if streamName == cfg.Name {
+			return true
+		}
+	}
+
+	return false
+}
+
+func createOrUpdateStream(ctx context.Context, js jetstream.JetStream, cfg jetstream.StreamConfig) error {
+	if streamExists(ctx, js, cfg) {
+		_, err := js.UpdateStream(ctx, cfg)
+
+		return oops.Wrapf(err, "error updating stream")
+	}
+
+	_, err := js.CreateStream(ctx, cfg)
+
+	return oops.Wrapf(err, "error creating stream")
+}
+
+func ConsumeActivity(ctx context.Context, nc *nats.Conn, consumerName string, subjects []string, handler func(msg jetstream.Msg) error) error {
+	return consume(ctx, nc, consumerName, "ACTIVITY", subjects, handler)
+}
+
+func consume(ctx context.Context, nc *nats.Conn, consumerName string, streamName string, subjects []string, handler func(msg jetstream.Msg) error) error {
+	slog.Info(fmt.Sprintf("%s is now running. Press CTRL-C to exit.", consumerName))
+	defer Close(nc)
+
+	js, err := jetstream.New(nc)
+	if err != nil {
+		return oops.Wrapf(err, "error creating jetstream instance")
+	}
+
+	stream, err := js.Stream(ctx, streamName)
+	if err != nil {
+		return oops.
+			With("consumer", consumerName).
+			With("stream", streamName).
+			Wrapf(err, "error creating stream")
+	}
+
+	dur, err := durableConsumer(ctx, consumerName, stream, subjects)
+
+	if err != nil {
+		return oops.Wrapf(err, "error creating durable consumer")
+	}
+
+	for {
+		msgs, errFetch := dur.Fetch(1)
+
+		if errFetch != nil {
+			slog.Error("error fetching", slog.String("consumer", consumerName), slog.Any("error", oops.Wrap(errFetch)))
+			time.Sleep(sleepBetweenConsumeFailure)
+			continue
+		}
+
+		select {
+		case <-ctx.Done():
+			return nil
+		case msg := <-msgs.Messages():
+			if msg != nil {
+				errHandler := handler(msg)
+
+				if errHandler != nil {
+					slog.Error("error consumer handler", slog.String("consumer", consumerName), slog.Any("error", errHandler))
+					time.Sleep(sleepBetweenConsumeFailure)
+
+					continue
+				}
+
+				errAck := msg.Ack()
+				if errAck != nil {
+					slog.Error("error consumer ack", slog.String("consumer", consumerName), slog.Any("error", errAck))
+				}
+			}
+		}
+	}
+}
+
+func durableConsumer(ctx context.Context, consumerName string, stream jetstream.Stream, subjects []string) (jetstream.Consumer, error) {
+	dur, errCreateConsumer := stream.CreateOrUpdateConsumer(ctx, jetstream.ConsumerConfig{
+		Durable:        consumerName,
+		DeliverPolicy:  jetstream.DeliverNewPolicy,
+		FilterSubjects: subjects,
+	})
+
+	if errCreateConsumer != nil {
+		return nil, oops.
+			With("consumer", consumerName).
+			Wrapf(errCreateConsumer, "error creating consumer")
+	}
+
+	return dur, nil
 }
